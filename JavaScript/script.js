2c5b82e3579040305e87cc609b2600cd765258c8
Akshansh246 <akshanshsaraf40@gmail.com>
2025-11-13 15:35:51 +0530

    prompt handling

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+let num = prompt('num btao: ');
+
+if (num === null) {
+    console.log("Cancel daba diya !!")
+}
+else{
+    if (num.trim() === '') {
+        console.log("Bhaii dhang se likh le !!")
+    }
+}
