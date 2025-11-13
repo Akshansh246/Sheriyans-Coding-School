1f91565add859f708737d5fc417334ad81838f6f
Akshansh246 <akshanshsaraf40@gmail.com>
2025-11-13 16:11:21 +0530

    Voting checker

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,28 @@
-let num = prompt('num btao: ');
+let age = prompt('num btao: ');
 
-if (num === null) {
+if (age === null) {
     console.error("Cancel daba diya !!")
 }
 else{
-    if (num.trim() === '') {
+    if (age.trim() === '') {
         console.error("Bhaii dhang se likh le !!")
+    }
+    else{
+        age = Number(age);
+        if (isNaN(age)) {
+            console.error("Number maanga tha bhaii");
+        }
+        else{ 
+            if(age<0){
+                console.error("minus m age kb se hone lgi ?");
+            } 
+            else if(age > 110){
+                console.error("Tum zinda kese ho abhi tak ?")
+            } 
+            else if (age>=18) {
+                console.log("Bhaii tu vote de skta haii");
+            }
+            else console.log("Beta tum bacche ho abhi")
+        }
     }
 }
