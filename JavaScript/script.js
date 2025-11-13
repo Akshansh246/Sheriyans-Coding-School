04d6696e43863f8f1ebd0ec26b780f66772e4f8e
Akshansh246 <akshanshsaraf40@gmail.com>
2025-11-13 17:08:55 +0530

    Password checker with 3 attempts

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,28 +1,21 @@
-let age = prompt('num btao: ');
+let attempts = 0
+let password = "akshubhai";
+let khulgya = false;
 
-if (age === null) {
-    console.error("Cancel daba diya !!")
+let pass = prompt("password btao !!")
+attempts++;
+if (pass === password) khulgya = true
+
+while (password !== pass) {
+    if (attempts === 3) {
+        console.error("Account Blocked");
+        break;
+    }
+    if(pass === password) khulgya = true
+    pass = prompt("password btao !!")
+    attempts++;
 }
-else{
-    if (age.trim() === '') {
-        console.error("Bhaii dhang se likh le !!")
-    }
-    else{
-        age = Number(age);
-        if (isNaN(age)) {
-            console.error("Number maanga tha bhaii");
-        }
-        else{ 
-            if(age<0){
-                console.error("minus m age kb se hone lgi ?");
-            } 
-            else if(age > 110){
-                console.error("Tum zinda kese ho abhi tak ?")
-            } 
-            else if (age>=18) {
-                console.log("Bhaii tu vote de skta haii");
-            }
-            else console.log("Beta tum bacche ho abhi")
-        }
-    }
+
+if (khulgya === true) {
+    console.log("Account unlocked")
 }
