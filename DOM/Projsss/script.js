8bd49d4f8f1fc7b4caa8b27802b394cb6bcda1b3
Akshansh246 <akshanshsaraf40@gmail.com>
2025-11-22 09:55:54 +0530

    custom cursor

--- DOM/Projsss/script.js
+++ DOM/Projsss/script.js
@@ -1,17 +1,8 @@
-let h5 = document.querySelector('h5')
-let addFriend = document.querySelector('#add');
-let check = 0
+let main = document.querySelector('main')
+let crsr = document.querySelector('.cursor')
 
-addFriend.addEventListener('click', function () {
-    if (check === 0) {
-        h5.innerHTML = 'Following';
-        h5.style.color = 'green';
-        addFriend.innerHTML = 'Unfollow'
-        check = 1;
-    }else{
-        h5.innerHTML = 'Singer - songwriter';
-        h5.style.color = 'black';
-        addFriend.innerHTML = 'Follow'
-        check = 0;
-    }
+main.addEventListener('mousemove', function (val){
+    console.log(val)
+    crsr.style.top = val.y+'px'
+    crsr.style.left = val.x+'px'
 })
